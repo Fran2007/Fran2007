72714e35d6b686ce64d400fdd018553087fdd1c5
Fran2007 <96666345+Fran2007@users.noreply.github.com>
2022-08-08 19:57:15 -0600

    Button

---
+++ codigo.js
@@ -0,0 +1,16 @@
+const button = document.querySelector('.buton');
+const contenedor1 = document.querySelector('.Contenedor-1');
+const contenedor2 = document.querySelector('.Contenedor-2');
+
+//button.addEventListener('mouseenter', (e)=>{
+      // alert('Di click en un boton')
+//});
+
+//contenedor1.addEventListener('keypress', (e)=>{
+     // alert('Di click en el contenedor ')
+       //e.stopPropagation();
+//},true);
+
+//contenedor2.addEventListener('mouseleave',(e)=>{
+      // alert('Di click en el contenedor azul')
+//})
